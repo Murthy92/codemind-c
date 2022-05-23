9cd9cbcbd33bc525d3f7b81fff0b7c29a4edd6c0
Murthy92 <97426500+Murthy92@users.noreply.github.com>
2022-05-23 07:14:58 +0530

    #include<stdio.h> int main() {     int num;     scanf("%d",&num);     if(num==1)     {         printf("0");     }     else if(num==2)     {         printf("1");     }     else     {         printf("2");     } }

---
+++ The_Pandemic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num==1)
+    {
+        printf("0");
+    }
+    else if(num==2)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("2");
+    }
+}
